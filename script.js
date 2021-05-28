82bbbcd6bf05805942cc54bd0c48f74697857f1c
phichakkhana chanthubluang <phichakkhana_ch@cmu.ac.th>
2021-05-28 13:26:40 +0700

    coomit 5

--- script.js
+++ script.js
@@ -36,12 +36,23 @@ function operate(op_obj) {
     stored = current;
     current = "";
 }
+function operate2(new_obj) {
+    nnew = new_obj.value;
+    if (nnew.indexOf("xy") > -1) { operation = 5; }
+    if (nnew.indexOf("xrooty") > -1) { operation = 6; }
+    if (nnew.indexOf("logyx") > -1) { operation = 7; }
+    stored = current;
+    current = "";
+}
 function calculate() {
     tmp = current;
     if (operation == 1) { current = eval(stored) + eval(current); };
     if (operation == 2) { current = eval(stored) - eval(current); };
     if (operation == 3) { current = eval(stored) * eval(current); };
     if (operation == 4) { current = eval(stored) / eval(current); };
+    if (operation == 5) { current = Math.pow(eval(stored), eval(current)) };
+    if (operation == 6) { current = Math.sqrt(eval(stored), (eval(current))) };
+    if (operation == 7) { current = Math.log(eval(current)) / Math.log(eval(stored)); };
     if (current.toString().length > MaxLength) {
         current = "Too long";
     }
@@ -80,6 +91,53 @@ function baselog() {
     current = Math.log(current);
     document.getElementById("display").innerHTML = current;
 }
+function e() {
+    current = current * Math.E;
+    document.getElementById("display").innerHTML = current;
+}
+function pi() {
+    current = current * Math.PI;
+    document.getElementById("display").innerHTML = current;
+}
+function rand() {
+    current = Math.random();
+    document.getElementById("display").innerHTML = current;
+}
+function factorial() {
+    const number = current;
+
+    // checking if number is negative
+    if (current < 0) {
+        current = "Error!"
+    }
+
+    // if number is 0
+    else if (number === 0) {
+        number = 1
+    }
+
+    // if number is positive
+    else {
+        let fact = 1;
+        for (i = 1; i <= number; i++) {
+            fact *= i;
+        }
+        current = fact;
+    }
+    document.getElementById("display").innerHTML = current;
+}
+function sumx() {
+    if (current < 0) {
+        current = "Error!";
+    } else {
+        let sum = 0;
+        for (i = 0; i <= current; i++) {
+            sum = sum + i;
+        }
+        current = sum;
+    }
+    document.getElementById("display").innerHTML = current;
+}
 document.getElementById("clear").addEventListener("click", clearDisp);
 document.getElementById("dot").addEventListener("click", addDot);
 document.getElementById("eval").addEventListener("click", calculate);
@@ -91,6 +149,11 @@ document.getElementById("10x").addEventListener("click", x10);
 document.getElementById("1/x").addEventListener("click", x1);
 document.getElementById("2√x").addEventListener("click", xroot2);
 document.getElementById("lnx").addEventListener("click", baselog);
+document.getElementById("e").addEventListener("click", e);
+document.getElementById("pi").addEventListener("click", pi);
+document.getElementById("fact").addEventListener("click", factorial);
+document.getElementById("sumx").addEventListener("click", sumx);
+document.getElementById("primex").addEventListener("click", primex);
 let all_num = document.getElementsByClassName("num")
 for (let i = 0; i < all_num.length; i++) {
     all_num[i].addEventListener("click", function () { addDigit(this); });
@@ -98,4 +161,8 @@ for (let i = 0; i < all_num.length; i++) {
 let all_ops = document.getElementsByClassName("ops")
 for (let i = 0; i < all_ops.length; i++) {
     all_ops[i].addEventListener("click", function () { operate(this); });
+}
+let all_new = document.getElementsByClassName("new")
+for (let i = 0; i < all_new.length; i++) {
+    all_new[i].addEventListener("click", function () { operate2(this); });
 }
